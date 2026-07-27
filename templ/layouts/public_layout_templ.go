ac874de490eb77870d87d998cd0d46e298cfe053
Muh Al Asfahani <asfah21@gmail.com>
2026-07-28 06:38:47 +0800

    [navbar] fix #2

--- templ/layouts/public_layout_templ.go
+++ templ/layouts/public_layout_templ.go
@@ -50,7 +50,7 @@ func PublicLayout(title string, body tc.Component) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<main>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<main class=\"public-content\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
